8d5020259542f4450f4d3f90faddee2598089093
Hannah Collins <hannahcollins53@gmail.com>
2023-05-31 08:27:31 -0700

    changed authorized

--- Off-The-Beaten-Path/src/community/UserProvider.js
+++ Off-The-Beaten-Path/src/community/UserProvider.js
@@ -1,9 +1,10 @@
 const localUser = localStorage.getItem("hike_user")
     const user = JSON.parse(localUser)
-    const token = user["token"]
+    
    
 
 export const getUser = (hikeUser) => {
+  const token = user["token"]
     return fetch(`https://beatenpath-app-pvyzi.ondigitalocean.app/users/${hikeUser}`, {
       headers: {
         "Accept": "application/json",
@@ -12,6 +13,7 @@ export const getUser = (hikeUser) => {
                 .then(res => res.json())
 }
 export const getUsers = () => {
+  const token = user["token"]
     return fetch(`https://beatenpath-app-pvyzi.ondigitalocean.app/users`, {
       headers: {
         "Accept": "application/json",
@@ -20,6 +22,7 @@ export const getUsers = () => {
                 .then(res => res.json())
 }
 export const getUserProfile = (userId) => {
+  const token = user["token"]
     return fetch(`https://beatenpath-app-pvyzi.ondigitalocean.app/userprofiles?user=${userId}`, {
             headers: {
               "Accept": "application/json",
@@ -28,10 +31,12 @@ export const getUserProfile = (userId) => {
                 .then(res => res.json())
 }
 export const getUserById = (id) => {
+  const token = user["token"]
     return fetch(`https://beatenpath-app-pvyzi.ondigitalocean.app/users/${id}`)
                 .then(res => res.json())
 }
 export const getUserProfileById = (id) => {
+  const token = user["token"]
     return fetch(`https://beatenpath-app-pvyzi.ondigitalocean.app/userprofiles?user=${id}`, {
       headers: {
         "Accept": "application/json",
